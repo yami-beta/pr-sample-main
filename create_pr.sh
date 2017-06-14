799156f2a80fcc123816a3f6930e878cd21b6211
yami_beta <yami-beta@users.noreply.github.com>
2017-06-14 14:30:47 +0900

    Add submodule

--- create_pr.sh
+++ create_pr.sh
@@ -4,13 +4,14 @@ pwd
 which git
 
 pushd ..
-git clone git@github.com:yami-beta/pr-sample-sub
+git clone --recursive git@github.com:yami-beta/pr-sample-sub
 pushd ./pr-sample-sub
 
 git config --local user.name "yami_beta"
 git config --local user.email "yami-beta@users.noreply.github.com"
 git checkout -b update-submodule
-git commit --allow-empty -m ':up: Update submodule'
+git submodule foreach git pull origin master
+git commit -m ':up: Update submodule'
 git push origin update-submodule
 
 popd
