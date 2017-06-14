94497afe6de2e77dbe38bde9d0b92cf74c39fc65
yami_beta <yami-beta@users.noreply.github.com>
2017-06-14 14:23:09 +0900

    Add git user config

--- create_pr.sh
+++ create_pr.sh
@@ -7,6 +7,8 @@ pushd ..
 git clone git@github.com:yami-beta/pr-sample-sub
 pushd ./pr-sample-sub
 
+git config --local user.name "yami_beta"
+git config --local user.email "yami-beta@users.noreply.github.com"
 git checkout -b update-submodule
 git commit --allow-empty -m ':up: Update submodule'
 git push origin update-submodule
