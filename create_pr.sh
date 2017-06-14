909153a83a4567e1b666d2c4158a473ffc4743a6
yami_beta <yami-beta@users.noreply.github.com>
2017-06-14 14:06:53 +0900

    Add checkout

--- create_pr.sh
+++ create_pr.sh
@@ -2,3 +2,14 @@
 
 pwd
 which git
+
+pushd ..
+git clone yami-beta/pr-sample-sub
+pushd ./pr-sample-sub
+
+git checkout -b update-submodule
+git commit --allow-empty -m ':up: Update submodule'
+git push origin update-submodule
+
+popd
+popd
