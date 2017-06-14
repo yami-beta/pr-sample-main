841195bd6e65cf74a0c9edbf7763d3cb2c74e12f
yami_beta <yami-beta@users.noreply.github.com>
2017-06-14 17:49:04 +0900

    Use env variable

--- create_pr.sh
+++ create_pr.sh
@@ -9,11 +9,11 @@ pushd ./pr-sample-sub
 
 git config --local user.name "yami_beta"
 git config --local user.email "yami-beta@users.noreply.github.com"
-git checkout master
+git checkout ${CIRCLE_BRANCH}
 git submodule foreach git pull origin master
 git add -A
 git commit -m ':up: Update submodule'
-git push origin master
+git push origin ${CIRCLE_BRANCH}
 
 popd
 popd
